02b0435531e707ae7566707f47313133eb754efa
Damla Bortucen <damla.bortucen.24@ucl.ac.uk>
2026-07-09 10:27:35 +0100

    feat: add navigation - footer to navigate between home and about screen

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+      tabBarActiveTintColor: '#DB162F',
+      headerStyle: {
+        backgroundColor: '#d5d7d7',
+      },
+      headerShadowVisible: false,
+      tabBarStyle: {
+        backgroundColor: '#d5d7d7',
+      },
+    }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
--- src/app/(tabs)/about.tsx
+++ src/app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text>About screen for Rules of the Game</Text>
     </View>
   );
 }
@@ -13,8 +13,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
   },
 });
